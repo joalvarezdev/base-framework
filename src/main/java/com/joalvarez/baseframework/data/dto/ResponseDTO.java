3372d230e3a606d2a962e5a80eea4fd29c7feb39
joalvarezdev <alvarez.joaquinez@gmail.com>
2023-05-28 15:07:17 -0300

    feat: new dto for response to requests #1

---
+++ src/main/java/com/joalvarez/baseframework/data/dto/ResponseDTO.java
@@ -0,0 +1,46 @@
+package com.joalvarez.baseframework.data.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.util.List;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ResponseDTO implements BaseDTO {
+
+    private Integer code;
+    private String message;
+    private List<String> details;
+
+    public ResponseDTO() {}
+
+    public ResponseDTO(Integer code, String message, List<String> details) {
+        this.code = code;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<String> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<String> details) {
+        this.details = details;
+    }
+}
